884efab2ef0572f9cc77a70784d1ac2b4bd5c30d
saurabhkumar.spsu@gmail.com <saurabhkumar.spsu@gmail.com>
2023-07-12 08:04:06 -0700

    typo fix

--- api/models/healthModel.js
+++ api/models/healthModel.js
@@ -2,7 +2,7 @@
 const dbHelper = require('../helpers/sqlHelper');
 const components = [
   {
-    name: 'mongoDB',
+    name: 'sql-database',
     live: dbHelper.live,
     ready: dbHelper.ready
   }
